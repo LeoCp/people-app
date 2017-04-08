9828698ad85473f05fc5e97dbd57c874dee47b73
Leonardo <leonardocolodette0@gmail.com>
2017-04-08 15:23:21 -0300

    setup graphql

---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+const app = express();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
