61581e44a102d693c5a991495385912bb6c67039
Leonardo <leonardocolodette0@gmail.com>
2017-04-08 18:02:31 -0300

    create ModalPerson component

---
+++ src/app/components/modal/ModalPerson.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react';
+
+const ModalPerson = ({ isOpenModal, toggle }) => {
+    return(
+      <Modal isOpen={this.state.modal} toggle={this.toggle} >
+        <ModalHeader toggle={this.toggle}>Create people</ModalHeader>
+        <ModalBody>
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Name:</label>
+              <input type="email" className="form-control"  placeholder="Name"/>
+            </div>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Age:</label>
+              <input type="email" className="form-control"  placeholder="Age"/>
+            </div>
+            <div className="form-group">
+              <label for="exampleInputEmail1">City:</label>
+              <input type="email" className="form-control"  placeholder="City"/>
+            </div>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          <Button color="primary" onClick={this.toggle}>Save</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+export default ModalPerson;
